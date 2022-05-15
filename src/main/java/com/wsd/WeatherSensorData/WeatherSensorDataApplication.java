c0c709c5ff93528a2b4fe6a1ce34285e8af6ac1f
Joe Moloney <joe.moloney496@gmail.com>
2022-05-15 14:59:02 +0100

    Initial Commit

---
+++ src/main/java/com/wsd/WeatherSensorData/WeatherSensorDataApplication.java
@@ -0,0 +1,13 @@
+package com.wsd.WeatherSensorData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSensorDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherSensorDataApplication.class, args);
+	}
+
+}
